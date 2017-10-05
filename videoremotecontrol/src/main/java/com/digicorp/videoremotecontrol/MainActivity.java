7878274d5f6eb616b255661813e3c21444bdad16
Kevin Adesara <kevin.adesara@digi-corp.com>
2017-10-05 12:21:09 +0530

    Added data collection fields in Remote app.

--- videoremotecontrol/src/main/java/com/digicorp/videoremotecontrol/MainActivity.java
+++ videoremotecontrol/src/main/java/com/digicorp/videoremotecontrol/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.gson.JsonObject;
 import com.koushikdutta.async.future.FutureCallback;
@@ -13,14 +15,15 @@ import com.koushikdutta.ion.Ion;
 public class MainActivity extends AppCompatActivity {
 
     private static final String API_BASE_URL = "http://192.168.0.30:3000";
-    private static final String API_START_RECORDING = "/start-over";
-    private static final String API_END_RECORDING = "/end-over";
+    private static final String API_START_OVER = "/start-over";
+    private static final String API_END_OVER = "/end-over";
 
+    private EditText txtTeamA;
+    private EditText txtTeamB;
+    private EditText txtOver;
+    private EditText txtScore;
     private Button btnStartRecording;
     private Button btnStopRecording;
-
-    private JsonObject matchInfo = new JsonObject();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,24 +31,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnStartRecording = (Button) findViewById(R.id.btnStartRecording);
         btnStopRecording = (Button) findViewById(R.id.btnStopRecording);
+        txtTeamA = (EditText) findViewById(R.id.txtTeamA);
+        txtTeamB = (EditText) findViewById(R.id.txtTeamB);
+        txtOver = (EditText) findViewById(R.id.txtOver);
+        txtScore = (EditText) findViewById(R.id.txtScore);
         btnStopRecording.setEnabled(false);
-        prepareMatchData();
     }
 
-    private void prepareMatchData() {
+    private JsonObject prepareMatchData() {
+        JsonObject matchInfo = new JsonObject();
         matchInfo.addProperty("match_id", "10");
-        matchInfo.addProperty("team_a", "DG-Lion");
-        matchInfo.addProperty("team_b", "DG-Tiger");
-        matchInfo.addProperty("score", "100 / 3");
-        matchInfo.addProperty("over", "10");
-        matchInfo.addProperty("batting", "DG-Lion");
+        matchInfo.addProperty("team_a", txtTeamA.getText().toString().trim().length() == 0 ? "DG-Lion" : txtTeamA.getText().toString().trim());
+        matchInfo.addProperty("team_b", txtTeamB.getText().toString().trim().length() == 0 ? "DG-Tiger" : txtTeamB.getText().toString().trim());
+        matchInfo.addProperty("score", txtScore.getText().toString().trim());
+        matchInfo.addProperty("over", txtOver.getText().toString().trim());
+        matchInfo.addProperty("batting", txtTeamA.getText().toString().trim());
+        return matchInfo;
     }
 
     public void startRecording(View view) {
+        JsonObject matchInfo = prepareMatchData();
         btnStartRecording.setEnabled(false);
         btnStopRecording.setEnabled(true);
         Ion.with(this)
-                .load(API_BASE_URL + API_START_RECORDING)
+                .load(API_BASE_URL + API_START_OVER)
                 .setJsonObjectBody(matchInfo)
                 .asJsonObject()
                 .setCallback(new FutureCallback<JsonObject>() {
@@ -53,20 +62,22 @@ public class MainActivity extends AppCompatActivity {
                     public void onCompleted(Exception e, JsonObject jsonObject) {
                         if (e != null) {
                             Log.d("START_RECORDING", "Error: " + e);
+                            Toast.makeText(MainActivity.this, "Error in start recording", Toast.LENGTH_SHORT).show();
                             return;
                         }
 
                         Log.d("START_RECORDING", "Response: " + jsonObject);
-
+                        Toast.makeText(MainActivity.this, "Recording started", Toast.LENGTH_SHORT).show();
                     }
                 });
     }
 
     public void stopRecording(View view) {
+        JsonObject matchInfo = prepareMatchData();
         btnStartRecording.setEnabled(true);
         btnStopRecording.setEnabled(false);
         Ion.with(this)
-                .load(API_BASE_URL + API_END_RECORDING)
+                .load(API_BASE_URL + API_END_OVER)
                 .setJsonObjectBody(matchInfo)
                 .asJsonObject()
                 .setCallback(new FutureCallback<JsonObject>() {
@@ -74,10 +85,12 @@ public class MainActivity extends AppCompatActivity {
                     public void onCompleted(Exception e, JsonObject jsonObject) {
                         if (e != null) {
                             Log.d("END_RECORDING", "Error: " + e);
+                            Toast.makeText(MainActivity.this, "Error in stop recording", Toast.LENGTH_SHORT).show();
                             return;
                         }
 
                         Log.d("END_RECORDING", "Response: " + jsonObject);
+                        Toast.makeText(MainActivity.this, "Recording stopped", Toast.LENGTH_SHORT).show();
                     }
                 });
     }
